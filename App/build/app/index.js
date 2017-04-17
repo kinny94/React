9f33167aaa984e41f9c9c3fbf1e64748967f89cc
Arjun  Dass <ad.sam007@gmail.com>
2017-04-16 19:08:29 -0500

    Closure

--- App/build/app/index.js
+++ App/build/app/index.js
@@ -1,18 +1,11 @@
-function Wizard(name, house, pet){
-    this.name = name;
-    this.house = house;
-    this.pet = pet;
-
-    this.read = () => `I'm ${this.name} from ${this.house}`;
+let call = () => {
+     let secret = "ES6 Rocks";
+     let reveal = () => {
+        console.log(secret);
+     }
+     return reveal;
 }
-
-Wizard.prototype.pet_name;
-Wizard.prototype.info = function() {
-    console.log(`I have a ${this.pet} name ${this.pet_name}`)
-};
-
-let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
-harry.pet_name = "Hedwig";
-console.log(harry);
-console.log(harry.read()); 
-console.log(harry.info());
+ 
+call(); 
+let unveil = call();
+unveil();
