4ebd7bfa19f38f352a823ba460b4ab104247aaa7
Arjun  Dass <ad.sam007@gmail.com>
2017-06-10 14:46:46 -0500

    First app

--- Hello React/public/app.jsx
+++ Hello React/public/app.jsx
@@ -1,55 +1,82 @@
+var GreeterMessage = React.createClass({
+    render: function(){
+
+    var name = this.props.name;
+    var p = this.props.message;
+
+        return(
+            <div>
+                <h1>Hello {name} !!</h1>
+                <p>{p} </p>
+            </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    
+    onFormSubmit: function(e){
+        e.preventDefault(); 
+
+        var updates = {};
+        var name = this.refs.name.value;
+        var message = this.refs.message.value;
+
+        if(name.length > 0){
+            this.refs.name.value = '';
+            updates.name = name;
+        }
+
+        if(message.length > 0){
+            this.refs.message.value = '';
+            updates.message = message;
+        }
+
+        this.props.onNewData(updates);
+    },
+
+    render: function(){
+        return(
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <input type="text" ref= "name" />
+                    <input type="text" ref="message" />
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+});
+
 var Greeter = React.createClass({
 
     getDefaultProps: function(){
         return{
             name: 'React!!',
             message: 'Default message!!',
-            errorText: ''
         }
     },
 
     getInitialState: function (){
-        return {
-            name: this.props.name
+        return { 
+            name: this.props.name,
+            message: this.props.message
         };
     },
 
-    onButtonClick: function (e) {
-        e.preventDefault();
-
-        var nameRef = this.refs.name;
-        var name = nameRef.value;
-        nameRef.value = '';
-        var errorText = '';
-
-        if(typeof name === 'string' && name.length > 0){
-            this.setState({
-                name: name,
-                errorText: ''
-            });
-        }else{
-            this.setState({
-                errorText: "Error Input"
-            });
-        }
+    handleUpdatedData: function (updates) {
+        this.setState(updates);
     },
 
     render: function(){
 
         var name = this.state.name;
-        var message = this.props.message;
-        var errorText = this.state.errorText;
+        var message = this.state.message;
 
         return (
-            <div>
-                <h1>I am {name}!</h1>
-                <p>{message}</p>
-
-                <form onSubmit={this.onButtonClick}>
-                    <input type="text" ref="name" />
-                    <button>Set Name </button>
-                </form>
-                <p>{errorText}</p>
+            <div> 
+                <GreeterMessage name={name} message={message}/>
+                <GreeterForm onNewData={this.handleUpdatedData}/>
             </div>    
         );
     }
