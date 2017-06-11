6b681af31edc8e7b6de9cfc28275bda94538dbc3
Arjun  Dass <ad.sam007@gmail.com>
2017-06-11 13:11:43 -0500

    Started a weather app with boilerPlate

---
+++ Weather-App/app/components/Nav.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var Nav = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h1>Navigation Bar!!</h1>
+            </div>
+        );
+    }
+});
+
+module.exports = Nav;   
