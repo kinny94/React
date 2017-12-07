73cbc8798a816fa9c56230e81181a42a3967d319
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-12-06 22:50:30 -0500

    using onclick

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,34 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+	
+
+	//Whereas props are set and passed by outside, state is changed by inside the component.
+	// state is only provided by component and is a reserved word
+	state = {
+		person: [
+			{ name:"Max", age:28},
+			{ name:"Manu", age: 26},
+			{ name:"Stephanie", age: 27}
+		]
+	}
+
+	switchNameHandler = () => {
+		console.log("Was Clicked");
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I am a react app</h1>
 				<p>This is really working!</p>
-				<Person name="Max" age="28"/>
-				<Person name="Manu" age="19">My Hobbies : Racing</Person>
-				<Person name="Stephanie" age="27"/>	
+
+				<button onClick={ this.switchNameHandler }>Switch Name</button>
+
+				<Person name={ this.state.person[0].name } age={ this.state.person[0].age } />
+				<Person name={ this.state.person[1].name } age={ this.state.person[1].age }>My Hobbies : Racing</Person>
+				<Person name={ this.state.person[2].name } age={ this.state.person[2].age } />	
+
 			</div>
 		);
 	}
