dad8cd959e7a522b92fe3664040425b1e09b9fe3
kinny94 <ad.sam007@gmail.com>
2017-12-22 19:28:13 -0500

    Adding dynamic styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
 	render() {
 
 		const style = {
-			backgroundColor: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
@@ -81,6 +82,8 @@ class App extends Component {
 					}
 				</div>
 			);
+
+			style.backgroundColor = 'red';
 		}
 
 		return (
