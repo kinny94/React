87fc5e9e5755ddcbff42dde1ddc4c1f63085ce08
kinny94 <ad.sam007@gmail.com>
2017-12-24 16:10:11 -0500

    Fixed Error

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => 
-    props.persons.map((per, index) => {
-        return 
-            <Person 
-                name={per.name} 
-                age={per.age} 
-                click={() => props.clicked(index)} 
-                key={per.id} 
-                changed={(event) => this.nameChangeHandler(event, per.id)
-            }/>
+const persons = (props) => props.persons.map((per, index) => {
+        return <Person 
+            name={per.name} 
+            age={per.age} 
+            click={() => props.clicked(index)} 
+            key={per.id} 
+            changed={(event) => props.changed(event, per.id)}
+        />
     });
   // default ES6  return statement structure
 
