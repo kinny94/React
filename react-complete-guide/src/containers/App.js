e87e1916e54f96c5616066bc3521da410f032079
kinny94 <ad.sam007@gmail.com>
2017-12-29 19:57:48 -0500

    Another example of hoc

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import withClass from '../hoc/WithClass';
 import WithClass from '../hoc/WithClass';
 //import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const withClass = (props) => {
+const withClass = (props) => (
     <div className={ props.classes }>{ props.children }</div>
-};
+);
 
 export default withClass;
 
