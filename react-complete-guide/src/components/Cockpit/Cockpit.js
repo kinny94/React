f580214b8847db54de33db4c20cb23d9e2e58a83
kinny94 <ad.sam007@gmail.com>
2017-12-29 19:35:35 -0500

    Introducted hoc
    Introducted hoc

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from './Cockpit.css';
-
+import Aux from '../../hoc/Aux';
 const cockpit = ( props  ) => {
 
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(" ");
     }
 
     if (props.person.length <= 2){
@@ -19,14 +19,14 @@ const cockpit = ( props  ) => {
     }
 
     return (
-        <div className={ classes.Cockpit }>
+        <Aux>
             <h1>Hi, I am a react app</h1>
             <p className={ assignedClasses.join(' ') }>This is really working.! Yes till now</p>
-            
+        
             <button 
-            className = { btnClass }
-            onClick={props.clicked}>Toggle Person</button>  
-        </div>
+                className = { btnClass }
+                onClick={props.clicked}>Toggle Person</button>  
+        </Aux>
     );
 };
 
