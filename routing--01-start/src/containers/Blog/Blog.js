4070040b767b8523ec3585e82bfd4c437158c66b
kinny94 <ad.sam007@gmail.com>
2018-01-03 19:35:56 -0500

    Switching components with reloading

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import axios from '../../axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import { Route } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     
@@ -19,8 +20,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={ () => <h1>Home</h1>  } />
-                <Route path="/" exact render={ () => <h1>Home 2</h1>  } />
+                { /* <Route path="/" exact render={ () => <h1>Home</h1>  } />
+                <Route path="/" exact render={ () => <h1>Home 2</h1>  } /> */  }
+                <Route path="/" exact component={ Posts } /> 
+                <Route path="/new-post" component={ NewPost } />
             </div>
         );
     }
