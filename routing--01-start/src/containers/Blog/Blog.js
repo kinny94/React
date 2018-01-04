e003df7d2a473caa320bf08e039da32fc4cd7d6d
kinny94 <ad.sam007@gmail.com>
2018-01-03 20:20:13 -0500

    Loading specific id routes

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 import { Route, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     
@@ -33,6 +34,7 @@ class Blog extends Component {
                 <Route path="/" exact render={ () => <h1>Home 2</h1>  } /> */  }
                 <Route path="/" exact component={ Posts } /> 
                 <Route path="/new-post" component={ NewPost } />
+                <Route path="/:id" exact component={ FullPost } />
             </div>
         );
     }
