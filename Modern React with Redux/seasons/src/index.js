f1662b53c63035132810877603a915199ae9aa59
Arjun Dass <ad.sam007@gmail.com>
2021-06-06 08:58:16 -0700

    use lifecycle  methods and css

--- Modern React with Redux/seasons/src/index.js
+++ Modern React with Redux/seasons/src/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: null };
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
     render() {
+
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: { this.state.errorMessage }</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: { this.state.lat }</div>
+            return <SeasonDisplay lat={ this.state.lat } />
         }
 
         return <div>Getting your latitude....</div>
