a6d924481a5c3abbfa6d83c5e540eabf4d3fada9
Arjun Dass <ad.sam007@gmail.com>
2021-06-09 20:31:37 -0700

    completing season display app

--- Modern React with Redux/seasons/src/index.js
+++ Modern React with Redux/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -13,8 +14,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
-
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: { this.state.errorMessage }</div>
         }
@@ -23,7 +23,15 @@ class App extends React.Component {
             return <SeasonDisplay lat={ this.state.lat } />
         }
 
-        return <div>Getting your latitude....</div>
+        return <Spinner message="Please accept localion request.."/>
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                { this.renderContent() }
+            </div>
+        )
     }
 }
 
