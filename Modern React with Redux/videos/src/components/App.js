086f9cc3f4a66320a3859e6eccb42a01807e26df
Arjun Dass <ad.sam007@gmail.com>
2021-06-16 22:22:09 -0700

    project setup

---
+++ Modern React with Redux/videos/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Search Bar</div>
+        );
+    }
+}
+
+export default App;
