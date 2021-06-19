31a9f502bcd636a6ea77e4c2436049202ccddd87
Arjun Dass <ad.sam007@gmail.com>
2021-06-19 13:18:28 -0700

    add styling and video components

--- Modern React with Redux/videos/src/components/VideoItem.js
+++ Modern React with Redux/videos/src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     return (
-        <div>
-            <img src={ video.snippet.thumbnails.medium.url } alt={ video.snippet.title }/>
+        <div className="video-item item" onClick={ () => onVideoSelect(video) }>
+            <img className="ui image" src={ video.snippet.thumbnails.medium.url } alt={ video.snippet.title }/>
             <div className="content">
                 <div className="header">{ video.snippet.title }</div>
             </div>
