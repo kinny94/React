6de2bc572a88851026e79b02887a1781c4c9833a
Arjun Dass <ad.sam007@gmail.com>
2021-06-20 22:21:30 -0700

    widgets project for react hooks

---
+++ Modern React with Redux/widgets/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a well known library among developers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'We use react by developings components'
+    }
+]
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return (
+        <div>
+            <Accordion items={ items }/>
+        </div>
+    );
+};
