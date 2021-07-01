d106bb5ce0c086043716f68e29354dff12c51ad6
Arjun  Dass <ad.sam007@gmail.com>
2021-06-30 21:21:24 -0700

    final redux store - codepen

--- Modern React with Redux/videos/src/redux.js
+++ Modern React with Redux/videos/src/redux.js
@@ -1,72 +1,88 @@
-// Actions
+console.clear();
+
+// People dropping off a form (Action Creators)
 const createPolicy = (name, amount) => {
-    return {
-        type: 'CREATE_POLICY',
-        payload: {
-            name,
-            amount
-        }
-    };
+  return { // Action (a form in our analogy)
+    type: 'CREATE_POLICY',
+    payload: {
+      name: name,
+      amount: amount
+    }
+  };
 };
 
 const deletePolicy = (name) => {
-    return {
-        type: "DELETE_POLICY",
-        payload: {
-            name
-        }
-    };
-};
-
-const createClaim = (name, amount) => {
-    return {
-        type: "CREATE_CLAIM",
-        payload: {
-            name,
-            amountOfMoneyToCollect: amount
-        }
-    };
-};
-
-// Reducers 
-const claimsHistory = (oldListOfClaims = [], action) => {
-    if (action.type === 'CREATE_CLAIM') {
-        return [...oldListOfClaims, action.payload];
+  return {
+    type: 'DELETE_POLICY',
+    payload: {
+      name: name
     }
+  };
+};
 
-    return oldListOfClaims;
-}
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: 'CREATE_CLAIM',
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect
+    }
+  };
+};
+
+
+// Reducers (Departments!)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    // we care about this action (FORM!)
+    return [...oldListOfClaims, action.payload];
+  }
+  
+  // we don't care the action (form!!)
+  return oldListOfClaims;
+};
 
 const accounting = (bagOfMoney = 100, action) => {
-    if (action.type === 'CREATE_CLAIM') {
-        return bagOfMoney - action.payload.amountOfMoneyToCollect;
-    } else if (action.type === 'CREATE_POLICY') {
-        return bagOfMoney + action.payload.amountOfMoneyToCollect;
-    } 
-
-    return bagOfMoney;
-}
+  if (action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amount;
+  }
+  
+  return bagOfMoney;
+};
 
 const policies = (listOfPolicies = [], action) => {
-    if (action.type === 'CREATE_POLICY') {
-        return [...listOfPolicies, action.payload.name];
-    } else if (action.type === "DELETE_POLICY") { 
-        return listOfPolicies.filter(name => name !==action.payload.name);
-    }
+  if (action.type === 'CREATE_POLICY') {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === 'DELETE_POLICY') {
+    return listOfPolicies.filter(name => name !== action.payload.name);
+  }
+  
+  return listOfPolicies;
+};
 
-    return listOfPolicies;
-} 
- 
-let Redux = {}; // just for testing, on codepen you will assign it to the actual redux library
 const { createStore, combineReducers } = Redux;
 
-const outDepartments = combineReducers({
-    accounting: accounting,
-    claimsHistory: claimsHistory, 
-    policies: policies
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
 });
 
-const store = createStore(outDepartments);
-// store.dispath();
-// store.getState()
+const store = createStore(ourDepartments);
 
+createPolicy('Alex', 20)
+createClaim('Alex', 120)
+deletePolicy('Alex')
+
+store.dispatch(createPolicy('Alex', 20));
+store.dispatch(createPolicy('Jim', 30));
+store.dispatch(createPolicy('Bob', 40));
+
+// store.dispatch(createClaim('Alex', 120));
+// store.dispatch(createClaim('Jim', 50));
+
+// store.dispatch(deletePolicy('Bob'));
+
+console.log(store.getState());
